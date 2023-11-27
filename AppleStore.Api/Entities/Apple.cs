237e961797dcb8c150273fd3c0e9b2ae49610271
kenya194 <eamankwah698@gmail.com>
2023-11-27 15:24:19 +0000

    apple class

--- AppleStore.Api/Entities/Apple.cs
+++ AppleStore.Api/Entities/Apple.cs
@@ -2,5 +2,10 @@ namespace AppleStore.Api.Entities;
 
 public class Apple
 {
-    
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Storage { get; set; }
+    public decimal Price { get; set; }
+    public required string ImageUri { get; set; }
+
 }
--- AppleStore.Api/Program.cs
+++ AppleStore.Api/Program.cs
@@ -1,6 +1,44 @@
+using AppleStore.Api.Entities;
+
+List<Apple> apples = new()
+{
+    new Apple()
+    {
+        Id = 1,
+        Name = "Iphone X",
+        Storage = "64gb",
+        Price = 2500.00M,
+        ImageUri = "https://placehold.co/100"
+    },
+    new Apple()
+    {
+        Id = 2,
+        Name = "Iphone X",
+        Storage = "256gb",
+        Price = 2700.00M,
+        ImageUri = "https://placehold.co/100"
+    },
+    new Apple()
+    {
+        Id = 3,
+        Name = "Iphone XR",
+        Storage = "64gb",
+        Price = 2600.00M,
+        ImageUri = "https://placehold.co/100"
+    },
+    new Apple()
+    {
+        Id = 4,
+        Name = "Iphone XR",
+        Storage = "128gb",
+        Price = 2800.00M,
+        ImageUri = "https://placehold.co/100"
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/apples", () => apples);
 
 app.Run();
